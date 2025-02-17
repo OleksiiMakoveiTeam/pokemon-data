63961fceebf95987f4b2d5da53f6aa56b46c7134
OleksiiMakoveiTeam <oleksiym81@gmail.com>
2025-02-17 14:18:36 +0200

    feat: add 404 page test

--- src/pages/404/404-page.tsx
+++ src/pages/404/404-page.tsx
@@ -1,8 +1,10 @@
 import { Stack, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
 
+// Since this is going to be rendered within the NavLayout, we don't need to add a header
 export const NotFoundPage = () => {
   const { t } = useTranslation();
+
   return (
     <Stack>
       <Typography>{t("NotFoundPage.message")}</Typography>
